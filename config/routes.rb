8350aeb30913fef7b390a04bfeeb18747b2e9994
HieuCR7 <hieuhv97@gmail.com>
2018-08-28 16:33:55 +0700

    login and remember_me

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
+  root "home#index"
+
   namespace :admin do
     root "dashboard#index"
   end
 
-  root "home#index"
-
   get "/signup", to: "users#signup"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :account_activations, only: :edit
+
 end
