d232f8ae34cb8bc89212be903e266df629dc2f2e
Le Duc Son <ducson.aptech@gmail.com>
2018-09-01 12:44:23 +0700

    Management users admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,54 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :load_user, except: %i(index new create)
+
+  def index
+    @users = User.newest.page(params[:page]).per(Settings.user_per)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:info] = t ".create_success"
+      redirect_to admin_users_path
+    else
+      flash[:danger] = t ".create_error"
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:info] = t ".update_success"
+      redirect_to admin_users_path
+    else
+      flash[:danger] = t ".update_error"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:info] = t ".destroy_success"
+    else
+      flash[:danger] = t ".destroy_error"
+    end
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def load_user
+    @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit :email, :fullname, :gender, :activated,
+      :admin, :money, :password, :password_confirmation
+  end
+end
