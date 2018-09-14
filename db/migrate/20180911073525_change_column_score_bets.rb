f556776972e2d0d2e90efdd151aeee9114247b5d
Le Duc Son <ducson.aptech@gmail.com>
2018-09-14 09:37:36 +0700

    edit migration change string->integer using for heroku

--- db/migrate/20180911073525_change_column_score_bets.rb
+++ db/migrate/20180911073525_change_column_score_bets.rb
@@ -1,5 +1,5 @@
 class ChangeColumnScoreBets < ActiveRecord::Migration[5.2]
   def change
-    change_column :score_bets, :status, :integer
+    change_column :score_bets, :status, :integer, using: "status::integer"
   end
 end
